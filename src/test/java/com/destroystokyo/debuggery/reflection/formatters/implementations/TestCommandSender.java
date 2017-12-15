9924752882e3fd82ce1be8b0a4efa816a39d74dd
Zach Brown <zach.brown@destroystokyo.com>
2017-12-15 18:41:56 -0500

    Relicense to GPLv3
    We were using the Linux kernels GPLv2 shouty license, I see no reason to
    continue to do so

--- src/test/java/com/destroystokyo/debuggery/reflection/formatters/implementations/TestCommandSender.java
+++ src/test/java/com/destroystokyo/debuggery/reflection/formatters/implementations/TestCommandSender.java
@@ -2,8 +2,9 @@
  * This file is part of Debuggery.
  *
  * Debuggery is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * distributed with this repository.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * Debuggery is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Debuggery. If not, see <http://www.gnu.org/licenses/>.
+ * along with Debuggery.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.destroystokyo.debuggery.reflection.formatters.implementations;
